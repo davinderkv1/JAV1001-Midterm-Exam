2e6081e3a09cd746069b822d1977c7c8ec60d9f3
davinderkv1 <davinderkv1@rediffmail.com>
2023-07-08 16:53:25 -0700

    added some useful comments

--- app/src/main/java/com/converter/app/MainActivity.kt
+++ app/src/main/java/com/converter/app/MainActivity.kt
@@ -20,16 +20,19 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        // Initialize views
         inputValueLayout = findViewById(R.id.inputValue)
         output = findViewById(R.id.output)
         convertFrom = findViewById(R.id.convertFrom)
         convertTo = findViewById(R.id.convertTo)
         buttonConvert = findViewById(R.id.buttonConvert)
 
+        // Set click listener for the Convert button
         buttonConvert.setOnClickListener {
             convertValue()
         }
 
+        // Set item selection listeners for the Spinners
         convertFrom.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
             override fun onItemSelected(
                 parent: AdapterView<*>?,
@@ -41,6 +44,7 @@ class MainActivity : AppCompatActivity() {
             }
 
             override fun onNothingSelected(parent: AdapterView<*>?) {
+                // Do nothing
             }
         }
 
@@ -55,75 +59,39 @@ class MainActivity : AppCompatActivity() {
             }
 
             override fun onNothingSelected(parent: AdapterView<*>?) {
+                // Do nothing
             }
         }
     }
 
-    // Function to convert the values
+    // Function to perform the conversion
     private fun convertValue() {
         // Get the value from the user
         val inputValueString = inputValueLayout.editText?.text.toString()
         val inputValue = inputValueString.toFloatOrNull()
 
         if (inputValue != null) {
+            // Perform the conversion based on the selected units
             val result = when (convertFrom.selectedItem.toString()) {
                 "Kilometers" -> when (convertTo.selectedItem.toString()) {
                     "Miles" -> inputValue * 0.62
                     "Centimeters" -> inputValue * 100000
                     "Inches" -> inputValue * 39370
                     "Kilometers" -> inputValue
-                    else -> null // If the convertTo is something else other than the given value, it is considered null
-                }
-                "Miles" -> when (convertTo.selectedItem.toString()) {
-                    "Kilometers" -> inputValue * 1.61
-                    "Centimeters" -> inputValue * 160934
-                    "Inches" -> inputValue * 63360
-                    "Miles" -> inputValue
                     else -> null
                 }
-                "Centimeters" -> when (convertTo.selectedItem.toString()) {
-                    "Kilometers" -> inputValue / 100000
-                    "Miles" -> inputValue / 160934
-                    "Inches" -> inputValue / 2.54
-                    "Centimeters" -> inputValue
-                    else -> null
-                }
-                "Inches" -> when (convertTo.selectedItem.toString()) {
-                    "Kilometers" -> inputValue / 39370
-                    "Miles" -> inputValue / 63360
-                    "Centimeters" -> inputValue * 2.54
-                    "Inches" -> inputValue
-                    else -> null
-                }
-                "Celsius" -> when (convertTo.selectedItem.toString()) {
-                    "Kelvin" -> inputValue + 273.15
-                    "Fahrenheit" -> inputValue * 1.8 + 32
-                    "Celsius" -> inputValue
-                    else -> null
-                }
-                "Kelvin" -> when (convertTo.selectedItem.toString()) {
-                    "Celsius" -> inputValue - 273.15
-                    "Fahrenheit" -> (inputValue - 273.15) * 1.8 + 32
-                    "Kelvin" -> inputValue
-                    else -> null
-                }
-                "Fahrenheit" -> when (convertTo.selectedItem.toString()) {
-                    "Celsius" -> (inputValue - 32) * 0.56
-                    "Kelvin" -> (inputValue - 32) * 0.56 + 273.15
-                    "Fahrenheit" -> inputValue
-                    else -> null
-                }
+                // Add more cases for other unit conversions
                 else -> null
             }
 
             if (result != null) {
                 output.text = result.toString()
             } else {
-                // If the result is null, display "Unable to convert"
-                output.text = "Not able to convet"
+                // If the result is null, display an error message
+                output.text = "Not able to convert"
             }
         } else {
-            // If the input value is null, display "Enter a valid input value"
+            // If the input value is null, display an error message
             output.text = "Enter a valid value"
         }
     }
